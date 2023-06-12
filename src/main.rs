4d13e4d8f739919954878e514e9074a2185fa2d3
Wonder0729 <135030265+Wonder0729@users.noreply.github.com>
2023-06-13 05:09:02 +0900

    implement GET shopping_list_items

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
-use warp::Filter;
+use std::{collections::HashMap, convert::Infallible, sync::Arc};
+use tokio::sync::Mutex;
+use warp::{Filter, Rejection};
+
+mod handlers;
+mod models;
+
+type ItemsDb = Arc<Mutex<HashMap<usize, models::ShoppingListItem>>>;
+type Result<T> = std::result::Result<T, Rejection>;
 
 #[tokio::main]
 async fn main() {
-    let root = warp::path::end().map(|| "Welcome to my warp server!");
-    let routes = root.with(warp::cors().allow_any_origin());
+    let items_db: ItemsDb = Arc::new(Mutex::new(HashMap::new()));
 
+    let root = warp::path::end().map(|| "Welcome to my warp server!");
+    let shopping_list_items_route = warp::path("shopping_list_items")
+        .and(warp::get())
+        .and(with_items_db(items_db.clone()))
+        .and_then(handlers::get_shopping_list_items);
+    
+    let routes = root
+        .or(shopping_list_items_route)
+        .with(warp::cors().allow_any_origin());
+    
     warp::serve(routes).run(([127, 0, 0, 1], 5000)).await;
+}
+
+fn with_items_db(
+    items_db: ItemsDb,
+) -> impl Filter<Extract = (ItemsDb,), Error = Infallible> + Clone {
+    warp::any().map(move || items_db.clone())
 }
