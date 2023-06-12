ee08aa43696daffd6f5c993173b408d06878af17
Wonder0729 <135030265+Wonder0729@users.noreply.github.com>
2023-06-13 05:32:35 +0900

    added single item create, get

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,25 @@ async fn main() {
     let items_db: ItemsDb = Arc::new(Mutex::new(HashMap::new()));
 
     let root = warp::path::end().map(|| "Welcome to my warp server!");
+
     let shopping_list_items_route = warp::path("shopping_list_items")
         .and(warp::get())
         .and(with_items_db(items_db.clone()))
         .and_then(handlers::get_shopping_list_items);
-    
+
+    let shopping_list_item_route = warp::path("shopping_list_item")
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(with_items_db(items_db.clone()))
+        .and_then(handlers::create_shopping_list_item)
+            .or(warp::path!("shopping_list_item" / usize)
+            .and(warp::get())
+            .and(with_items_db(items_db.clone()))
+            .and_then(handlers::get_shopping_list_item_by_id));
+
     let routes = root
         .or(shopping_list_items_route)
+        .or(shopping_list_item_route)
         .with(warp::cors().allow_any_origin());
     
     warp::serve(routes).run(([127, 0, 0, 1], 5000)).await;
